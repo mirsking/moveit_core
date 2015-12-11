80c3057ef8f4f4db66545c2a855b81b9eac56f9e
mirsking <mirsking@gmail.com>
2015-12-11 14:02:24 +0800

    add install build-essential script

---
+++ get_necessary.sh
@@ -0,0 +1,5 @@
+sudo apt-get -y install ros-indigo-moveit-msgs
+sudo apt-get -y install ros-indigo-object-recognition-msgs
+sudo apt-get -y install ros-indigo-octomap-msgs
+sudo apt-get -y install ros-indigo-srdfdom
+sudo apt-get -y install ros-indigo-fcl
